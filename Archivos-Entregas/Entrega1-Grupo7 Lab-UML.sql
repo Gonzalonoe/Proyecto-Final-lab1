6eb8367b45e0b6882d961c12a1e453ac558ba253
Cadelago Nicolas <163204206+nicokevin08@users.noreply.github.com>
2024-06-08 19:35:20 -0300

    entrega1

---
+++ Archivos-Entregas/Entrega1-Grupo7 Lab-UML.sql
@@ -0,0 +1,189 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-06-2024 a las 23:06:16
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `solbuslinealapunta`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `colectivos`
+--
+
+CREATE TABLE `colectivos` (
+  `ID_Colectivo` int(11) NOT NULL,
+  `Matricula` varchar(10) NOT NULL,
+  `Marca` varchar(30) NOT NULL,
+  `Modelo` varchar(30) NOT NULL,
+  `Capacidad` int(11) NOT NULL,
+  `Estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horarios`
+--
+
+CREATE TABLE `horarios` (
+  `ID_Horario` int(11) NOT NULL,
+  `ID_Ruta` int(11) NOT NULL,
+  `Hora_Salida` time NOT NULL,
+  `Hora_Llegada` time NOT NULL,
+  `Estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pasaje`
+--
+
+CREATE TABLE `pasaje` (
+  `ID_Pasaje` int(11) NOT NULL,
+  `ID_Pasajero` int(11) NOT NULL,
+  `ID_Colectivo` int(11) NOT NULL,
+  `ID_Ruta` int(11) NOT NULL,
+  `Fecha_Viaje` date NOT NULL,
+  `Hora_Viaje` time NOT NULL,
+  `Asiento` int(11) NOT NULL,
+  `Precio` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pasajero`
+--
+
+CREATE TABLE `pasajero` (
+  `ID_Pasajero` int(11) NOT NULL,
+  `Nombre` varchar(20) NOT NULL,
+  `Apellido` varchar(20) NOT NULL,
+  `DNI` varchar(20) NOT NULL,
+  `Correo` varchar(30) NOT NULL,
+  `Telefono` varchar(20) NOT NULL,
+  `Estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rutas`
+--
+
+CREATE TABLE `rutas` (
+  `ID_Rutas` int(11) NOT NULL,
+  `Origen` varchar(50) NOT NULL,
+  `Destino` varchar(50) NOT NULL,
+  `Duracion_Estimada` time NOT NULL,
+  `Estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `colectivos`
+--
+ALTER TABLE `colectivos`
+  ADD PRIMARY KEY (`ID_Colectivo`),
+  ADD UNIQUE KEY `Matricula` (`Matricula`);
+
+--
+-- Indices de la tabla `horarios`
+--
+ALTER TABLE `horarios`
+  ADD PRIMARY KEY (`ID_Horario`),
+  ADD UNIQUE KEY `ID_Ruta` (`ID_Ruta`);
+
+--
+-- Indices de la tabla `pasaje`
+--
+ALTER TABLE `pasaje`
+  ADD PRIMARY KEY (`ID_Pasaje`),
+  ADD UNIQUE KEY `ID_Pasajero` (`ID_Pasajero`),
+  ADD UNIQUE KEY `ID_Colectivo` (`ID_Colectivo`),
+  ADD UNIQUE KEY `ID_Ruta` (`ID_Ruta`);
+
+--
+-- Indices de la tabla `pasajero`
+--
+ALTER TABLE `pasajero`
+  ADD PRIMARY KEY (`ID_Pasajero`),
+  ADD UNIQUE KEY `DNI` (`DNI`),
+  ADD UNIQUE KEY `Correo` (`Correo`);
+
+--
+-- Indices de la tabla `rutas`
+--
+ALTER TABLE `rutas`
+  ADD PRIMARY KEY (`ID_Rutas`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pasaje`
+--
+ALTER TABLE `pasaje`
+  MODIFY `ID_Pasaje` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pasajero`
+--
+ALTER TABLE `pasajero`
+  MODIFY `ID_Pasajero` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rutas`
+--
+ALTER TABLE `rutas`
+  MODIFY `ID_Rutas` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `horarios`
+--
+ALTER TABLE `horarios`
+  ADD CONSTRAINT `horarios_ibfk_1` FOREIGN KEY (`ID_Ruta`) REFERENCES `rutas` (`ID_Rutas`);
+
+--
+-- Filtros para la tabla `pasaje`
+--
+ALTER TABLE `pasaje`
+  ADD CONSTRAINT `pasaje_ibfk_2` FOREIGN KEY (`ID_Colectivo`) REFERENCES `colectivos` (`ID_Colectivo`),
+  ADD CONSTRAINT `pasaje_ibfk_3` FOREIGN KEY (`ID_Pasajero`) REFERENCES `pasajero` (`ID_Pasajero`);
+
+--
+-- Filtros para la tabla `rutas`
+--
+ALTER TABLE `rutas`
+  ADD CONSTRAINT `rutas_ibfk_1` FOREIGN KEY (`ID_Rutas`) REFERENCES `pasaje` (`ID_Ruta`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
